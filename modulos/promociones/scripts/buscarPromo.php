8bd549762b88479c5c9964fdbedab4e74882b245
Franco Viale <francoviale@eximben.cl>
2024-06-13 13:13:32 -0400

    fix buscar promo Ñ

--- modulos/promociones/scripts/buscarPromo.php
+++ modulos/promociones/scripts/buscarPromo.php
@@ -1,18 +1,31 @@
 <?php
 require_once("../../../clases/conexionocdb.php");
+
 $sql = "SELECT ALU,convert(int,PRICE01) precio,isnull(descuento,'NO')descuento,DESC2 FROM [RP_VICENCIO].[dbo].[RP_Articulos] art
 LEFT JOIN [RP_VICENCIO].[dbo].[RP_Descuento_Navidad2] nav ON art.ALU =nav.upc
 where alu ='".$_POST['codigo']."'";
 
-$rsSql = odbc_exec( $conn, $sql);
-	if (!$rsSql){  
-		exit( "Error en la consulta SQL" );
-	}	
-	$resultado = odbc_fetch_array($rsSql);
-	$objeto->precio=$resultado['precio'];
-	$objeto->descripcion=$resultado['DESC2'];
-	$objeto->oferta=$resultado['descuento'];
-	echo json_encode($objeto);
+// Escapa el valor de $_POST['codigo'] para evitar inyecciones SQL
+//$codigo = odbc_escape_string($_POST['codigo']);
 
-odbc_close( $conn );
+$sql = "SELECT ALU, convert(int, PRICE01) precio, ISNULL(descuento, 'NO') descuento, DESC2 
+        FROM [RP_VICENCIO].[dbo].[RP_Articulos] art
+        LEFT JOIN [RP_VICENCIO].[dbo].[RP_Descuento_Navidad2] nav ON art.ALU = nav.upc
+        WHERE alu = '$codigo'";
+//echo $sql;
+
+$rsSql = odbc_exec($conn, $sql);
+if (!$rsSql) {
+    exit("Error en la consulta SQL");
+}
+
+$resultado = odbc_fetch_array($rsSql);
+$objeto = new stdClass();
+$objeto->precio = utf8_encode($resultado['precio']);
+$objeto->descripcion = utf8_encode($resultado['DESC2']);
+$objeto->oferta = utf8_encode($resultado['descuento']);
+
+echo json_encode($objeto);
+
+odbc_close($conn);
 ?>
