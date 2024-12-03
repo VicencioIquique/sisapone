0d032ee5f0dab843c4214b022d315d5a35f1915d
fvialem <francoviale@eximben.cl>
2024-12-03 13:23:49 -0300

    add nov 2024 stats

--- clases/menu.php
+++ clases/menu.php
@@ -330,6 +330,7 @@ class menu{
 													 <li><a target'_blank' href='../sisapone/archivos/Ago2024.xlsx'>Ago. 2024</a></li>
 													 <li><a target'_blank' href='../sisapone/archivos/Sep2024.xlsx'>Sep. 2024</a></li>
 													 <li><a target'_blank' href='../sisapone/archivos/Oct2024.xlsx'>Oct. 2024</a></li>
+													 <li><a target'_blank' href='../sisapone/archivos/Nov2024.xlsx'>Oct. 2024</a></li>
 													</ul>
 												</li>
 										    
@@ -738,6 +739,7 @@ class menu{
 													 <li><a target'_blank' href='../sisapone/archivos/Ago2024.xlsx'>Ago. 2024</a></li>
 													 <li><a target'_blank' href='../sisapone/archivos/Sep2024.xlsx'>Sep. 2024</a></li>
 													 <li><a target'_blank' href='../sisapone/archivos/Oct2024.xlsx'>Oct. 2024</a></li>
+													 <li><a target'_blank' href='../sisapone/archivos/Nov2024.xlsx'>Oct. 2024</a></li>
 													</ul>
 												</li>
 										    </ul>
@@ -1281,6 +1283,7 @@ class menu{
 													 <li><a target'_blank' href='../sisapone/archivos/Ago2024.xlsx'>Ago. 2024</a></li>
 													 <li><a target'_blank' href='../sisapone/archivos/Sep2024.xlsx'>Sep. 2024</a></li>
 													 <li><a target'_blank' href='../sisapone/archivos/Oct2024.xlsx'>Oct. 2024</a></li>
+													 <li><a target'_blank' href='../sisapone/archivos/Nov2024.xlsx'>Oct. 2024</a></li>
 													</ul>
 												</li>
 										    </ul>
